f3934aa271bf6c21ba04af6bf37c127e17733ab9
codebyhimanshu <himanshusingh993567@gmail.com>
2025-09-30 15:47:11 +0530

    all sources allowed for images

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com", 
+        hostname: "**", 
       },
       {
-        protocol: "https",
-        hostname: "plus.unsplash.com", 
+        protocol: "http",
+        hostname: "cloudinary.com",
       },
       {
         protocol: "https",
