b41b011995bbb6eaa212c998b9c00ab1f5c6d0d5
ryaaan4321 <aryan056400@gmail.com>
2025-11-15 12:04:35 +0530

    feat: implement Google API integration for sending payment confirmation emails

---
+++ app/api/google/sendDetailsEmail/route.ts
@@ -0,0 +1,13 @@
+import { sendPaymentDetailsEmail } from "@/lib/sendPayementConfirmationEmail";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+export async function POST(req:NextRequest){
+    try{
+        const body=await req.json();
+        await sendPaymentDetailsEmail(body);
+        return NextResponse.json({success:true},{status:200});
+    }catch(e:any){
+        return NextResponse.json({success:false},{status:500});
+    }
+}
