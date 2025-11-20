c17dbe763a6243a44357fabefd96e0a6bea6f496
Amankhare01 <amankhare.aa@gmail.com>
2025-11-20 18:35:53 +0530

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import type { ReactNode } from "react";
 import "./globals.css";
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
@@ -10,6 +11,20 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           name="google-site-verification"
           content="KMzlY12tp_yI_3dXiRVxcPqC2e6JHqJdOvdyTSTGXoY"
         />
+        {/* Google Tag (gtag.js) */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-FF1EX4EHJL"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-FF1EX4EHJL');
+        `}
+      </Script>
       </head>
       <body>{children}</body>
     </html>
